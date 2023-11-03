8c78dc2b6cc20948c33fbaff33a7d8ce7019d522
Elena V <elenaveremchuk@Elenas-MacBook-Pro.local>
2023-11-03 15:17:36 -0500

    created new files

---
+++ src/MyMealsAndIngridients.js
@@ -0,0 +1,38 @@
+const MyMealsAndIngridients = ({ selectedDay, updateDay }) => {
+
+    const editMyMeal = (myInput, value) => {
+        updateDay({
+            ...selectedDay,
+            [myInput]: value
+        })
+    }
+
+    if (!selectedDay) return ""
+
+    return <div className="whole-plan">
+        <input
+            type="text"
+            className="myInput"
+            placeholder="Today is..."
+            id="title"
+            value={selectedDay.title}
+            onChange={(e) => editMyMeal("title", e.target.value)}
+        />
+
+        <textarea
+            placeholder="Write your meal plan here"
+            id="mealForADay"
+            value={selectedDay.mealForADay}
+            onChange = {(e) => editMyMeal("mealForADay", e.target.value)}
+        />
+
+        <textarea
+            placeholder="List of ingridients"
+            id="mealForADay"
+            value={selectedDay.ingridients}
+            onChange = {(e) => editMyMeal("ingridients", e.target.value)}
+        />
+        </div>
+}
+
+export default  MyMealsAndIngridients;
